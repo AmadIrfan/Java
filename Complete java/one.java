5e62be025947b448e0de1ee47e9c548c5070037d
Amad Irfan <99482198+AmadIrfan@users.noreply.github.com>
2023-08-19 12:50:39 +0500

    new code file added

---
+++ Complete java/one.java
@@ -0,0 +1,10 @@
+// import java.util.Scanner;
+
+class one {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // scanner.close();
+
+        // System.out.println("AmadIrfan");
+    }
+}
--- first/src/Main.java
+++ first/src/Main.java
@@ -1,10 +1,12 @@
 import java.util.*;
+
 public class Main {
-    public  static  void main(String[] args){
- for (int i=0;i<10;i++){
-     System.out.println("Amad Irfan");
- }
- Scanner scanner=new Scanner(System.in);
- scanner.nextLine();
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Amad Irfan");
+        }
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        scanner.close();
     }
 }
